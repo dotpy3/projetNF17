cc8b1b2e929ebdb2fb665246b9b477f13680d665
nxp3 <moppapo@gmail.com>
2015-06-09 11:42:57 +0200

    Modifications projet

--- karate_kid/init.php
+++ karate_kid/init.php
@@ -6,15 +6,22 @@
 </head>
 <?php include("include/head.php");
 if (isset($_GET['error']) && $_GET['error'] = 'noFile') $error = "<h2>Erreur : Aucun fichier envoyé</h2>";
+if (isset($_GET['error']) && $_GET['error'] = 'noSelection') $error = "<h2>Erreur : Aucune sélection de type de fichier</h2>";
+if (isset($_GET['error']) && $_GET['error'] = 'invalidType') $error = "<h2>Erreur : Fichier de type incorrect</h2>";
 else $error = "";
 ?>
 <body>
 	<?php include("include/menu.php"); ?>
 	<h1>Initialiser la base de donnees</h1>
 	<?php echo $error; ?>
-	<form method="POST" action="init_action.php">
+	<form method="POST" action="init_action.php" enctype="multipart/form-data">
 		<table>
-			<tr><td>Fichier CSV ou XML :</td><td> <input type="file" name="file"/></td></tr>
+			<select name="choixInit">
+				<option value="kata">Initialiser les katas</option>
+				<option value="mvts">Initialiser les mouvements</option>
+				<option value="grades">Initialiser les grades du karaté</option>
+			</select>
+			<tr><td>Fichier CSV ou XML :</td><td> <input type="file" name="file" id="file"/></td></tr>
 		</table><br/>
 		<input class="button" type="reset" value="Effacer"/>
 		<input class="button" type="submit" value="Valider"/>
--- karate_kid/init_action.php
+++ karate_kid/init_action.php
@@ -1,4 +1,45 @@
 <?php
+include("include/head.php");
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
+
+/* FONCTIONS */
+
+function traitementCSV($fichier){
+
+}
+
+/* VÉRIFICATIONS DE TRANSFERT ET DE TYPE */
+
 if (!isset($_FILES['file'])) {
 	header('Location: init.php?error=noFile');
+}
+
+/* VERIFICATION DE LA SELECTION */
+
+$listeModifsPossibles = array('kata','mvts','grades');
+if (!isset($_POST['choixInit'])
+	|| !in_array($_POST['choixInit'], $listeModifsPossibles)){
+	header('Location: init.php?error=noSelection')
+}
+$choixTypeModif = $_POST['choixInit'];
+
+/* VERIFICATION DU TYPE */
+
+$fichier = $_FILES['file'];
+$type = $fichier["type"];
+if ($type != "text/csv" && $type != "text/xml") {
+	header('Location: init.php?error=invalidType');
+}
+$adresseFichier = $fichier['tmp_name'];
+$contenuFichier = file_get_contents($adresseFichier);
+
+/* DÉBUT DU TRAITEMENT DES DONNÉES */
+
+if ($type == "text/csv"){
+	// CSV
+	$tab = traitementCSV($adresseFichier,$choixTypeModif);
+} else {
+	// XML
+	$tab = traitementXML($contenuFichier,$choixTypeModif);
 }
