f67225cdcabcb4f813c91512f1ed486650fe65bf
Eric Gourlaouen <gourlaoueneric@gmail.com>
2015-06-16 20:53:21 +0200

    ajout add_match

--- karate_kid/add_match.php
+++ karate_kid/add_match.php
@@ -1,4 +1,25 @@
-<?php include("include/head.php"); ?>
+<?php include("include/head.php");
+$host='tuxa.sme.utc';
+	$port= '5432';
+	$dbname= 'dbnf17p146';
+	$user= 'nf17p146';
+	$password= 'htJ4IXmZ';
+	$GLOBALS['bdd'] = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+	?>
+<?php 
+function addOptionsToSelect($competitionType){
+	$query="SELECT * FROM ".$competitionType;
+	$reponse = pg_query($GLOBALS['bdd'],$query);
+	$echo = "<optgroup label='".$competitionType.">'";
+	while($data = pg_fetch_array($reponse,null,PGSQL_ASSOC)){
+		$echo.='<option value="'.$data['nom']."'>".$data['nom']."</option>";
+	}
+	$echo .= "</optgroup>";
+	return $echo;
+}
+
+
+
 <body>
 	<?php include("include/menu.php"); ?>
 	<h1>Ajouter un match</h1>
@@ -8,7 +29,10 @@
 			$reponse = pg_query($bdd,$query);
 			
 			echo "<select name='id_competition'>";
-			echo "<option>A COMPLÉTER</option>";
+			echo addOptionsToSelect("competition_katas");
+			echo addOptionsToSelect("competition_kumite");
+			echo addOptionsToSelect("competition_tameshi_wari");
+			echo addOptionsToSelect("competition_mixte");
 			echo "</select><br/>";
 		?>		
 		<input class="button" type="reset" value="Effacer"/>
