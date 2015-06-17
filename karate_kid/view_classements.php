bbbc807606b6ac5f99e29cf968f8fd8d815a3917
Winnilis <agathe.guillemot@gmail.com>
2015-06-17 20:02:29 +0200

    up

--- karate_kid/view_classements.php
+++ karate_kid/view_classements.php
@@ -1,11 +1,21 @@
 <?php include("include/head.php"); ?>
 <body>
 	<?php include("include/menu.php"); ?>
-	<h1>Classements</h1>
+	<h1>Compétitions</h1>
 	<form method="POST" action="view_classements_action.php">
-		<select name="nom_competition">
-			<option>Liste extraite des noms de compétition existants</option>
-		</select><br/>
+		<?php
+			$query = "SELECT * FROM competition_katas 
+					  UNION SELECT * FROM competition_kumite
+					  UNION SELECT * FROM competition_mixte
+					  UNION SELECT * FROM competition_tameshi_wari;"; //UNION de toutes les tables de compétitions
+			$reponse = pg_query($bdd,$query);
+			
+			echo "<select name='nom_competition'>";
+			while($data = pg_fetch_array($reponse)){
+				echo "<option value='".$data['nom']."'>".$data['nom']."</option>";
+			}
+			echo "</select><br/>";
+		?>		
 		<input class="button" type="reset" value="Effacer"/>
 		<input class="button" type="submit" value="Valider"/>
 	</form>
